a1a0cb3339ddf6ac5ae5b47eefd4f84e58283cb6
afterjs <ricardoamaroesm@gmail.com>
2021-10-11 02:55:16 +0100

    addded images to assets

--- app/src/main/java/ipvc/estg/calculator/MainActivity.kt
+++ app/src/main/java/ipvc/estg/calculator/MainActivity.kt
@@ -1,11 +1,10 @@
 package ipvc.estg.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
